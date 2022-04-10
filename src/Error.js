43ebcca1258d76d3ed226fc7f6b3cf35cf35041e
shonjmoj <53841190+Miid0@users.noreply.github.com>
2022-04-10 22:12:19 +0000

    Delete Error.js

--- src/Error.js
+++
@@ -1,9 +0,0 @@
-const Error = (props) => {
-    return (
-        <div className="m-2 text-gray-100 text-lg font-semibold">
-            <h2>{props.message}</h2>
-        </div>
-    );
-}
- 
-export default Error;
